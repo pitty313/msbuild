ded3e9a29c7a1918381b3274a9329dc59912d7e5
shiatiali <Baalbek.313@gmail.com>
2026-06-17 07:08:14 -0400

    chore: enable nullable reference types - core tasks and build modules

--- src/Tasks/IAnalyzerHostObject.cs
+++ src/Tasks/IAnalyzerHostObject.cs
@@ -1,24 +1,21 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Runtime.InteropServices;
 using Microsoft.Build.Framework;
 
-#nullable disable
+namespace Microsoft.Build.Tasks.Hosting;
 
-namespace Microsoft.Build.Tasks.Hosting
+/// <summary>
+/// Defines an interface for the Vbc/Csc tasks to communicate information about
+/// analyzers and rulesets to the IDE.
+/// </summary>
+[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+[ComVisible(true)]
+[Guid("B5A95716-2053-4B70-9FBF-E4148EBA96BC")]
+public interface IAnalyzerHostObject
 {
-    /// <summary>
-    /// Defines an interface for the Vbc/Csc tasks to communicate information about
-    /// analyzers and rulesets to the IDE.
-    /// </summary>
-    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-    [ComVisible(true)]
-    [Guid("B5A95716-2053-4B70-9FBF-E4148EBA96BC")]
-    public interface IAnalyzerHostObject
-    {
-        bool SetAnalyzers(ITaskItem[] analyzers);
-        bool SetRuleSet(string ruleSetFile);
-        bool SetAdditionalFiles(ITaskItem[] additionalFiles);
-    }
+    bool SetAnalyzers(ITaskItem[] analyzers);
+    bool SetRuleSet(string ruleSetFile);
+    bool SetAdditionalFiles(ITaskItem[] additionalFiles);
 }
